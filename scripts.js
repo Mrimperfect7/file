65a2140929f93872f1b983a2213814a32f8fdd1b
ATHUL <110906985+Mrimperfect7@users.noreply.github.com>
2024-01-21 09:43:14 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,19 +2,3 @@ function toggleDrawer() {
   var drawer = document.querySelector('.drawer');
   drawer.style.display = (drawer.style.display === 'block') ? 'none' : 'block';
 }
-feather.replace();
-
-const links = document.querySelectorAll('.navigation__link');
-
-links.forEach(link => {
-  link.addEventListener('click', e => {
-    e.preventDefault();
-    if (!link.classList.contains('active')) {
-      const active = document.querySelector('.navigation__link.active');
-      if (active !== null) {
-        active.classList.remove('active');
-      }
-      link.classList.add('active');
-    }
-  });
-});
